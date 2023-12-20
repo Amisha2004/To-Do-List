b3172fc8e4e4ac356ee07f07eeb0b5289f9d87d2
Amisha Kumari <104024062+Amisha2004@users.noreply.github.com>
2023-12-20 17:33:00 +0530

    Update note.js

--- public/JS/note.js
+++ public/JS/note.js
@@ -4,14 +4,18 @@ showNotes();
 // If user adds a note, add it to the localStorage
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
-  let addTxt = document.getElementById("addTxt");
   let addTitle = document.getElementById("addTitle");
+  let addTxt = document.getElementById("addTxt");
   let notes = localStorage.getItem("notes");
+  // notes= [{"title":"Amisha","text":"I am amisha"},{"title":"notes","text":"I am notes"}]
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
+  // the JSON string represents a simple object with properties "title" and "content".
+  // After using JSON.parse(), the variable parsedNotes now holds a JavaScript object with
+  // the same properties, and you can access its values using dot notation.
   let myObj = {
     title: addTitle.value,
     text: addTxt.value
@@ -20,7 +24,6 @@ addBtn.addEventListener("click", function(e) {
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
   addTitle.value = "";
-//   console.log(notesObj);
   showNotes();
 });
 
@@ -35,13 +38,13 @@ function showNotes() {
   let html = "";
   notesObj.forEach(function(element, index) {
     html += `
-      <div id="noteContainer_${index}" class="note_container" style="background-color: ${element.isImportant ? '#030f38' : '#030f38'}">
+      <div id="noteContainer_${index}" class="note_container" style="background-color: ${element.isImportant ? '#B22222' : '#030f38'};  margin-left: 10px; margin-bottom:10px;">
         <div class="card_container">
           <h5 class="card-title">${element.title}</h5>
           <p class="card-text">${element.text}</p>
           <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-danger">Delete Note</button>
           <button id="${index}" onclick="markAsImp(this.id)" class="btn btn-primary" style="background-color: ${
-      element.isImportant ? 'rgb(233, 101, 13)' : '#030f38'}">
+      element.isImportant ? '#030f38' : '#030f38'}">
             &#9733;
           </button>
         </div>
@@ -58,7 +61,6 @@ function showNotes() {
 // Function to delete a note
 function deleteNote(index) {
 //   console.log("I am deleting", index);
-
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
@@ -81,17 +83,18 @@ function markAsImp(index) {
   notesObj[index].isImportant = !notesObj[index].isImportant;
   localStorage.setItem("notes", JSON.stringify(notesObj));
 
-  let noteContainer = document.getElementById("noteContainer_" + index);
-  let button = document.getElementById(index);
+  // let noteContainer = document.getElementById("noteContainer_" + index);
+  // let button = document.getElementById(index);
 
-  if (notesObj[index].isImportant) {
-    noteContainer.style.backgroundColor = "#030f38";
-    button.style.backgroundColor = "rgb(233, 101, 13)";
-  } else {
-    noteContainer.style.backgroundColor = "";
-    button.style.backgroundColor = "";
-  }
+  // if (notesObj[index].isImportant) {
+  //   noteContainer.style.backgroundColor = "#030f38";
+  //   button.style.backgroundColor = "rgb(233, 101, 13)";
+  // } else {
+  //   noteContainer.style.backgroundColor = "";
+  //   button.style.backgroundColor = "";
+  // }
 }
+
 
 
 
